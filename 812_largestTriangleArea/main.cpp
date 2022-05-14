2cdadde3efda4156776f7a23488d14baadebef1b
yongjies <yongjies@cadence.com>
2022-05-15 07:31:59 +0800

    Initial Commit

---
+++ 812_largestTriangleArea/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  812_largestTriangleArea
+//
+//  Created by 孙永杰 on 2022/5/15.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
